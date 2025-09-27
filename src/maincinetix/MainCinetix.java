28abd9164211d156b8418a9dff00c79679dc8170
renm1 <renm1@Pavilon>
2025-09-26 18:57:54 -0700

    Primer commit chicos. Se vienen cositas.

---
+++ src/maincinetix/MainCinetix.java
@@ -0,0 +1,22 @@
+package maincinetix;
+import java.util.Scanner;
+public class MainCinetix {
+
+    public static Scanner lector = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        // Hola
+        System.out.println("Para que haya algo!");
+        System.out.println("Intoduce exactamente la palabra -- CINETIX --");
+        String input = lector.nextLine();
+        while(!input.equals("CINETIX")) {
+            System.out.println("Palabra incorrecta. Ingresa la palabra de nuevo o ingresa -- x -- para salir:");
+            input = lector.nextLine();
+            if (input.equals("x") || input.equals("CINETIX")) {
+                break;
+            }
+        }
+        System.out.println(input);
+    }
+    
+}
